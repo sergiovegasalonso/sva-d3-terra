352533eddff1c7ffb69a7cc0f422b7e23a255c40
Sergio Vegas Alonso <svegas@bilbomatica.es>
2023-03-09 00:25:13 +0100

    dfsa

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,11 @@
-const { json, select, selectAll, geoOrthographic, geoPath } = d3;
-let geoJsonData, globe, projection, path;
+const { json, select, selectAll, geoOrthographic, geoPath, geoGraticule } = d3;
+
+const globeSize = {
+    width: window.innerWidth / 3,
+    height: window.innerHeight,
+};
+
+let geoJsonData, globe, projection, path, graticule, infoPanel;
 
 json("https://terra.deno.dev/")
     .then(data => init(data));
@@ -7,23 +13,39 @@ json("https://terra.deno.dev/")
 const init = data => {
     geoJsonData = data;
     drawGlobe();
+    drawGraticule();
+    renderInfoPanel();
 };
 
 const drawGlobe = () => {
     globe = select("body")
         .append("svg")
 
-    projection = geoOrthographic();
-    path = geoPath().projection(projection);
+    projection = geoOrthographic()
+        .fitSize([globeSize.width, globeSize.height], geoJsonData)
+        .translate([window.innerWidth - globeSize.width / 1.5, window.innerHeight / 2]);
+    
+    path = geoPath()
+        .projection(projection);
 
-    globe
-        .selectAll("path")
+    globe.selectAll("path")
         .data(geoJsonData.features)
         .enter()
         .append("path")
         .attr("d", path)
         .style("fill", "#ffffff")
         .style("stroke", "#000000")
-        .attr("class", "country")
-
+        .attr("class", "country");
 };
+
+const drawGraticule = () => {
+    graticule = geoGraticule();
+
+    globe.append("path")
+        .attr("class", "graticula")
+        .attr("d", path(graticule()))
+        .style("fill", "none")
+        .style("stroke", "grey")
+};
+
+const renderInfoPanel = () => {};
